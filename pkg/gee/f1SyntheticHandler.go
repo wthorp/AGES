0e2e23b65985b200710239c8c49f672cec0d60fd
wthorp <bill3000@hotmail.com>
2019-02-02 08:14:30 -0500

    trying to fix fractal

--- pkg/gee/f1SyntheticHandler.go
+++ pkg/gee/f1SyntheticHandler.go
@@ -11,6 +11,7 @@ import (
 	"io/ioutil"
 	"math"
 	"net/http"
+	"sync"
 
 	"github.com/golang/protobuf/proto"
 )
@@ -90,37 +91,41 @@ func fractalHandler(w http.ResponseWriter, r *http.Request, quadkey string, vers
 
 	myimage := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{tileSize, tileSize}})
 
+	i := complex128(complex(0, 1))
+	zoom := float64(math.Pow(2, float64(tileZ-2)))
+	tileRange := 1 / zoom
+	tileStartX := 1/zoom + (tileRange * tileX)
+	tileStartY := 1/zoom + (tileRange * tileY)
+
 	// This loop just fills the image tile with fractal data
+	var wg sync.WaitGroup
+	wg.Add(tileSize)
 	for cx := 0; cx < tileSize; cx++ {
-		for cy := 0; cy < tileSize; cy++ {
-			i := complex128(complex(0, 1))
-			zoom := float64(math.Pow(2, float64(tileZ-2)))
+		go func(cx int) {
+			for cy := 0; cy < tileSize; cy++ {
+				x := -2 + tileStartX + (float64(cx)/tileSize)*tileRange
+				y := -2 + tileStartY + (float64(cy)/tileSize)*tileRange
 
-			tileRange := 1 / zoom
-			tileStartX := 1/zoom + (tileRange * tileX)
-			tileStartY := 1/zoom + (tileRange * tileY)
+				// x and y are now in the range ~-2 -> +2
+				z := complex128(complex(x, 0)) + complex128(complex(y, 0))*complex128(i)
 
-			x := -2 + tileStartX + (float64(cx)/tileSize)*tileRange
-			y := -2 + tileStartY + (float64(cy)/tileSize)*tileRange
+				c := complex(0.274, 0.008)
+				for n := 0; n < 100; n++ {
+					z = z*z + complex128(c)
+				}
 
-			// x and y are now in the range ~-2 -> +2
-			z := complex128(complex(x, 0)) + complex128(complex(y, 0))*complex128(i)
-
-			c := complex(0.274, 0.008)
-			for n := 0; n < 100; n++ {
-				z = z*z + complex128(c)
+				z = z * 10
+				ratio := float64(2 * (real(z) / 2))
+				r := math.Max(0, float64(255*(ratio-1)))
+				b := math.Max(0, float64(255*(1-ratio)))
+				g := float64(255 - b - r)
+				col := color.RGBA{uint8(r), uint8(g), uint8(b), 255}
+				myimage.Set(cx, cy, col)
+				wg.Done()
 			}
-
-			z = z * 10
-			ratio := float64(2 * (real(z) / 2))
-			r := math.Max(0, float64(255*(ratio-1)))
-			b := math.Max(0, float64(255*(1-ratio)))
-			g := float64(255 - b - r)
-			col := color.RGBA{uint8(r), uint8(g), uint8(b), 255}
-			myimage.Set(cx, cy, col)
-		}
+		}(cx)
 	}
-
+	wg.Wait()
 	writeJpeg(w, myimage)
 }
 
